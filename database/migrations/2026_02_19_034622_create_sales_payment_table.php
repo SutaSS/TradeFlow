0002f0b6b496c349a37f04210ab95a018e5bd688
Andikasaktih <andikasaktih@gmail.com>
2026-02-19 11:49:58 +0700

    refactor: add column sales payment table

--- database/migrations/2026_02_19_034622_create_sales_payment_table.php
+++ database/migrations/2026_02_19_034622_create_sales_payment_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,10 +19,25 @@ return new class extends Migration
             $table->unsignedBigInteger('paid_by');
             $table->string('status');
             $table->timestamps();
-            
+
             $table->foreign('sales_invoice_id')->references('sales_invoice_id')->on('sales_invoice');
             $table->foreign('received_by')->references('user_id')->on('m_user');
             $table->foreign('paid_by')->references('customer_id')->on('m_customer');
+
+            $table->string('payment_method')->default('manual');
+            // manual, xendit
+
+            $table->string('external_id')->nullable();
+            // id dari xendit
+
+            $table->string('reference_id')->nullable();
+            // reference invoice xendit
+
+            $table->string('payment_channel')->nullable();
+            // VA, Ewallet, etc
+
+            $table->string('gateway_status')->nullable();
+            // PENDING, PAID, EXPIRED
         });
     }
 
